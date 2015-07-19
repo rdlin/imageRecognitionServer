531b45fdc8c2e28b3b7076576a49bcf0fa2874ea
Robert Lin <rdlin@github.com>
2015-07-19 00:44:13 -0400

    Fixing bugs

--- hello.py
+++ hello.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/url', method="POST")
+@app.route('/url')
 def requestw():
-  url = request.args.get('url')
+  url = 'http://imgur.com/' + request.args.get('imgur')
   response = unirest.post("https://camfind.p.mashape.com/image_requests",
   headers={
     "X-Mashape-Key": "ysmAp4zJQcmshhUqiSlmhotB226Bp1fclLOjsnidCdRFmzIRI9",
@@ -22,13 +22,17 @@ def requestw():
     "image_request[locale]": "en_US",
     "image_request[remote_image_url]": url
   } )
-  token = response['token']
+  token = response.body['token']
   response2 = unirest.get("https://camfind.p.mashape.com/image_responses/{token}",
   headers={
     "X-Mashape-Key": "ysmAp4zJQcmshhUqiSlmhotB226Bp1fclLOjsnidCdRFmzIRI9",
     "Accept": "application/json"
   })
 
-  if response2['status'] is "completed":
+  if response2.body['status'] is "completed":
       return jsonify(response = response2['name'])
   return jsonify(response = 'none')
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run()
